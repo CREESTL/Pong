8bcd56911c795997a9859f0d76103bc3eee9d8aa
CREESTL <37812120+CREESTL@users.noreply.github.com>
2021-07-06 22:51:26 +0300

    Draw the First Paddle

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use tetra::graphics::{self, Color};
+// {self} here allows us to use simple 'graphics' but not 'terra::graphics' in the future
+use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
+use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
+
+
+
+const WINDOW_HEIGHT: f32 = 480.0;
+const WINDOW_WIDTH: f32 = 640.0;
+
 
 /*
 Context is a struct that holds all of the 'global' state managed by the framework, 
@@ -14,13 +23,40 @@ It exposes various methods that will be called during the game loop, and you can
 order to define your game's behaviour.
 */
 
-struct GameState {}
-impl State for GameState {}
+struct GameState {
+    paddle_texture: Texture
+}
+
+impl GameState {
+      // Constructor for game state
+    fn new(ctx: &mut Context) -> tetra::Result<GameState>{
+        // Just load the texture without drawing it
+        let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
+
+        Ok(GameState{paddle_texture})
+    }
+}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result{
+        // Fill the screen with background color
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        // Draw the first player paddle at position 16 16
+        self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
+
+        // It should return Result
+        Ok(())
+    }
+}
 
 fn main() -> tetra::Result{
-    ContextBuilder::new("Pong", 640, 480)
-    .quit_on_escape(true)
-    .build()?
-    .run(|_| {Ok(GameState{})})
+    // Create a game window
+    ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
+        // Close the windows when hitting Esc
+        .quit_on_escape(true)
+        // Build the whole game
+        .build()?
+        // Running the builder we create a new game state
+        .run(|ctx| GameState::new(ctx))
 
 }
