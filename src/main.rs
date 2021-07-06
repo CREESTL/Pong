50d8cf5c651e37934c7a9bd5c3b2a21cd8457f0f
CREESTL <37812120+CREESTL@users.noreply.github.com>
2021-07-06 23:26:24 +0300

    Add Second Player & His Movement

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,19 @@ const WINDOW_WIDTH: f32 = 640.0;
 // Paddle movement speed
 const PADDLE_SPEED: f32 = 8.0;
 
+// Holds info about game entity - paddle, ball or whatever else that has texture and position
+struct Entity {
+    texture: Texture,
+    position: Vec2<f32>,
+}
+
+impl Entity {
+    // Constructor of a new game entity
+    fn new(texture: Texture, position: Vec2<f32>) -> Entity{
+        Entity {texture, position}
+    }
+}
+
 /*
 Context is a struct that holds all of the 'global' state managed by the framework, 
 such as window settings and connections to the graphics/audio/input hardware. 
@@ -25,19 +38,34 @@ order to define your game's behaviour.
 */
 
 struct GameState {
-    paddle_texture: Texture,
-    paddle_position: Vec2<f32>,
+    player1: Entity,
+    player2: Entity,
 }
+
 
 impl GameState {
       // Constructor for game state
     fn new(ctx: &mut Context) -> tetra::Result<GameState>{
         // Just load the texture without drawing it
-        let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
-        // Set the paddle position
-        let paddle_position = Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
+        let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
+        // Set the player's position
+        let player1_position = Vec2::new(
+            16.0, (WINDOW_HEIGHT - player1_texture.height() as f32) / 2.0
+            );
+        // Create an entity of first player
+        let player1 = Entity::new(player1_texture, player1_position);
 
-        Ok(GameState{paddle_texture, paddle_position})
+        let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
+        let player2_position = Vec2::new(
+            WINDOW_WIDTH - player2_texture.width() as f32 - 16.0,
+            (WINDOW_HEIGHT - player2_texture.height() as f32) / 2.0,
+        );
+        let player2 = Entity::new(player2_texture, player2_position);
+
+        Ok(GameState{
+            player1,
+            player2,
+        })
     }
 }
 
@@ -46,8 +74,9 @@ impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result{
         // Fill the screen with background color
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
-        // Draw the first player paddle at position 16 16
-        self.paddle_texture.draw(ctx, self.paddle_position);
+        // Draw both players' paddles
+        self.player1.texture.draw(ctx, self.player1.position);
+        self.player2.texture.draw(ctx, self.player2.position);
 
         // It should return Result
         Ok(())
@@ -55,15 +84,24 @@ impl State for GameState {
 
     // Game state updating function. Works 60 times a second
     fn update(&mut self, ctx: &mut Context) -> tetra::Result{
-        // Move the paddle up when W is pressed
+        // Move the first player's paddle up when W is pressed
         if input::is_key_down(ctx, Key::W){
-            self.paddle_position.y -= PADDLE_SPEED;
+            self.player1.position.y -= PADDLE_SPEED;
         }
-        // Move the paddle down when S is pressed
+        // Move the first player's paddle down when S is pressed
         if input::is_key_down(ctx, Key::S){
-            self.paddle_position.y += PADDLE_SPEED;
+            self.player1.position.y += PADDLE_SPEED;
         }
 
+        // Move the second player's paddle up when UP arrow is pressed
+        if input::is_key_down(ctx, Key::O){
+            self.player2.position.y -= PADDLE_SPEED;
+        }
+
+        // Move the second player's paddle down when DOWN arrow is pressed
+        if input::is_key_down(ctx, Key::K){
+            self.player2.position.y += PADDLE_SPEED;
+        }
 
         Ok(())
     }
