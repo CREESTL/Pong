7e3f6dd86a8d531003fe9691ea642d3872aa6701
CREESTL <37812120+CREESTL@users.noreply.github.com>
2021-07-06 23:03:27 +0300

    Add Paddle Movement

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
 
-
+// Size of the window
 const WINDOW_HEIGHT: f32 = 480.0;
 const WINDOW_WIDTH: f32 = 640.0;
-
+// Paddle movement speed
+const PADDLE_SPEED: f32 = 8.0;
 
 /*
 Context is a struct that holds all of the 'global' state managed by the framework, 
@@ -24,7 +25,8 @@ order to define your game's behaviour.
 */
 
 struct GameState {
-    paddle_texture: Texture
+    paddle_texture: Texture,
+    paddle_position: Vec2<f32>,
 }
 
 impl GameState {
@@ -32,19 +34,37 @@ impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState>{
         // Just load the texture without drawing it
         let paddle_texture = Texture::new(ctx, "./resources/player1.png")?;
+        // Set the paddle position
+        let paddle_position = Vec2::new(16.0, (WINDOW_HEIGHT - paddle_texture.height() as f32) / 2.0);
 
-        Ok(GameState{paddle_texture})
+        Ok(GameState{paddle_texture, paddle_position})
     }
 }
 
 impl State for GameState {
+    // Rendering function
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result{
         // Fill the screen with background color
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
         // Draw the first player paddle at position 16 16
-        self.paddle_texture.draw(ctx, Vec2::new(16.0, 16.0));
+        self.paddle_texture.draw(ctx, self.paddle_position);
 
         // It should return Result
+        Ok(())
+    }
+
+    // Game state updating function. Works 60 times a second
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result{
+        // Move the paddle up when W is pressed
+        if input::is_key_down(ctx, Key::W){
+            self.paddle_position.y -= PADDLE_SPEED;
+        }
+        // Move the paddle down when S is pressed
+        if input::is_key_down(ctx, Key::S){
+            self.paddle_position.y += PADDLE_SPEED;
+        }
+
+
         Ok(())
     }
 }
